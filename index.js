8481ee62798cfd8ed90d08d5b5a57541669671ac
arrkiin <arrkiin@gmail.com>
2018-04-16 14:10:11 +0200

    Change createElement to createClass / include MouseEvents

--- index.js
+++ index.js
@@ -21,48 +21,85 @@
  *
  */
 
-'use strict';
+'use strict'
 
-const React = require('react');
-const PropTypes = require('prop-types');
+const React = require('react')
+const PropTypes = require('prop-types')
 
-function createElement(name, type) {
-  function CreateElement(props) {
-    return React.createElement(type, props, props.children);
-  }
-
-  CreateElement.displayName = name;
-
-  CreateElement.propTypes = {
-    children: PropTypes.node
-  };
-
-  CreateElement.defaultProps = {
-    children: undefined
-  };
-
-  return CreateElement;
+const EVENT_TRANSFORM = {
+  onPress: 'onClick',
+  onPressIn: 'onMouseDown',
+  onPressOut: 'onMouseUp',
 }
 
-const Svg = module.exports = createElement('Svg', 'svg');
+function prepareChildProps(props) {
+  let childProps = {
+    ...props
+  }
+  for (let key in EVENT_TRANSFORM) {
+    if (childProps[key]) {
+      if (!childProps[EVENT_TRANSFORM[key]]) {
+        childProps[EVENT_TRANSFORM[key]] = childProps[key]
+      }
+      delete childProps[key]
+    }
+  }
+  return childProps
+}
 
-Svg.Circle = createElement('Circle', 'circle');
-Svg.ClipPath = createElement('ClipPath', 'clipPath');
-Svg.Defs = createElement('Defs', 'defs');
-Svg.Ellipse = createElement('Ellipse', 'ellipse');
-Svg.G = createElement('G', 'g');
-Svg.Image = createElement('Image', 'image');
-Svg.Line = createElement('Line', 'line');
-Svg.LinearGradient = createElement('LinearGradient', 'linearGradient');
-Svg.Path = createElement('Path', 'path');
-Svg.Polygon = createElement('Polygon', 'polygon');
-Svg.Polyline = createElement('Polyline', 'polyline');
-Svg.RadialGradient = createElement('RadialGradient', 'radialGradient');
-Svg.Rect = createElement('Rect', 'rect');
-Svg.Stop = createElement('Stop', 'stop');
-Svg.Svg = createElement('Svg', 'svg');
-Svg.Symbol = createElement('Symbol', 'symbol');
-Svg.Text = createElement('Text', 'text');
-Svg.TextPath = createElement('TextPath', 'textPath');
-Svg.TSpan = createElement('TSpan', 'tspan');
-Svg.Use = createElement('Use', 'use');
+function createClass(name, type) {
+  return class extends React.Component {
+    static displayName = name
+    static propTypes = {
+      children: PropTypes.node,
+    }
+    static defaultProps = {
+      children: undefined,
+    }
+    setNativeProps = nativeProps => {
+      this.setState(prevState => {
+        return {
+          ...prevState,
+          nativeProps: nativeProps,
+        }
+      })
+    }
+    constructor(props) {
+      super(props)
+      this.state = {}
+    }
+    render() {
+      let childProps = prepareChildProps(this.props)
+      if (this.state.nativeProps) {
+        childProps = {
+          ...childProps,
+          ...this.state.nativeProps,
+        }
+      }
+      return React.createElement(type, childProps, this.props.children)
+    }
+  }
+}
+
+const Svg = (module.exports = createClass('Svg', 'svg'))
+
+Svg.Circle = createClass('Circle', 'circle')
+Svg.ClipPath = createClass('ClipPath', 'clipPath')
+Svg.Defs = createClass('Defs', 'defs')
+Svg.Ellipse = createClass('Ellipse', 'ellipse')
+Svg.G = createClass('G', 'g')
+Svg.Image = createClass('Image', 'image')
+Svg.Line = createClass('Line', 'line')
+Svg.LinearGradient = createClass('LinearGradient', 'linearGradient')
+Svg.Path = createClass('Path', 'path')
+Svg.Polygon = createClass('Polygon', 'polygon')
+Svg.Polyline = createClass('Polyline', 'polyline')
+Svg.RadialGradient = createClass('RadialGradient', 'radialGradient')
+Svg.Rect = createClass('Rect', 'rect')
+Svg.Stop = createClass('Stop', 'stop')
+Svg.Svg = createClass('Svg', 'svg')
+Svg.Symbol = createClass('Symbol', 'symbol')
+Svg.Text = createClass('Text', 'text')
+Svg.TextPath = createClass('TextPath', 'textPath')
+Svg.TSpan = createClass('TSpan', 'tspan')
+Svg.Use = createClass('Use', 'use')
